00e3df960b6cb0422d276c47601726109d3ef694
mathieu <m.drapeau9@gmail.com>
2017-08-25 21:25:16 -0400

    update

--- include/ehAppSample.h
+++ include/ehAppSample.h
@@ -1,18 +1,44 @@
 #include "ehLogger.h"
 
 #include <iostream>
+#include <cstdlib>
 
 namespace eh {
 
 struct App {
-
-  eh::Msg::Separator msgSeparator;
   struct Module : public eh::Logger::Module {
-    virtual std::string toString() const override { return ""; }
+    enum Level {
+      ERROR = 0,
+      INFO,
+      DEBUG
+    };
+
+    Level  m_level = Level::INFO;
+
+    Module() {}
+    Module(Module::Level level) : m_level(level) {}
+
+    virtual std::string toString() const override {
+      switch (m_level) {
+        case Level::ERROR:
+          return "ERROR:";
+        case Level::INFO:
+          return "INFO:";
+        case Level::DEBUG:
+          return "DEBUG:";
+      }
+      return "";
+    }
     virtual void overrideStream(std::ostream *&os) const override {
       os = &std::cout;
     }
-  } logModule;
+    virtual bool isActive() const override {
+      return m_level <= App::get().m_logModule.m_level;
+    }
+  };
+
+  Module m_logModule;
+  eh::Msg::Separator m_msgSeparator;
 
   static const App &get() {
     static App app;
@@ -20,13 +46,34 @@ struct App {
   }
 
 private:
-  App() {}
+  App() {
+    if (const char* levelEnv = std::getenv("EH_LOGGER_LEVEL")) {
+      const std::string levelStr = levelEnv;
+      try {
+        const int level = std::stoi(levelStr);
+        if (level >= Module::Level::ERROR && level <= Module::Level::DEBUG) {
+          m_logModule.m_level = static_cast<Module::Level>(level);
+        }
+      } catch (...) {
+      }
+    }
+  }
 
   App(const App &) = delete;
   App &operator=(const App &) = delete;
 };
 }
 
-#define LOG(...)                                                               \
-  (eh::Logger::log(eh::App::get().logModule, eh::App::get().msgSeparator,      \
-                   __VA_ARGS__, EH_BUILD_FILE_INFO));
+#define LOG_ERROR(...)                                             \
+  (eh::Logger::log(eh::App::Module(eh::App::Module::Level::ERROR), \
+                   eh::App::get().m_msgSeparator, __VA_ARGS__,     \
+                   EH_BUILD_FILE_INFO));
+#define LOG_INFO(...)                                             \
+  (eh::Logger::log(eh::App::Module(eh::App::Module::Level::INFO), \
+                   eh::App::get().m_msgSeparator, __VA_ARGS__,     \
+                   EH_BUILD_FILE_INFO));
+#define LOG_DEBUG(...)                                             \
+  (eh::Logger::log(eh::App::Module(eh::App::Module::Level::DEBUG), \
+                   eh::App::get().m_msgSeparator, __VA_ARGS__,     \
+                   EH_BUILD_FILE_INFO));
+
--- test/test_logger.cpp
+++ test/test_logger.cpp
@@ -2,6 +2,8 @@
 
 int main() {
   std::string test = "test";
-  LOG(test);
+  LOG_ERROR(test, "bla", 1);
+  LOG_INFO(test, "bla", 1);
+  LOG_DEBUG(test, "bla", 1);
   return 0;
 }
